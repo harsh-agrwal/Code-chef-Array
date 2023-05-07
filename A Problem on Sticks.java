93dc0ae2d88305d855de9efaa736d59e66d6fab9
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-05-07 13:11:15 +0530

    Create A Problem on Sticks.java

---
+++ A Problem on Sticks.java
@@ -0,0 +1,89 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    
+     static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+  
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+  
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+  
+        int nextInt() { return Integer.parseInt(next()); }
+  
+        long nextLong() { return Long.parseLong(next()); }
+  
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+  
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 FastReader sc = new FastReader();
+		int t=sc.nextInt();
+		while(t-->0){
+		    
+		    int n = sc.nextInt();
+		    int arr[] = new int[n];
+		    for(int i=0;i<n;i++){
+		        arr[i] = sc.nextInt();
+		    }
+		    Arrays.sort(arr);
+		    if(arr[n-1]==0){
+		         System.out.println(0);
+		        continue;
+		    }
+		    int ans= 1;
+		    for(int i=n-2;i>=0;i--){
+		        if(arr[i]==0){
+		            break;
+		        }
+		        if(arr[i]!=arr[i+1]){
+		            ans++;
+		        }
+		    }
+		    System.out.println(ans);
+		}
+	}
+}
