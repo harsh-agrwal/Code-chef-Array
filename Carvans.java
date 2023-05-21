29e15ac45f1d9a6dc8c327dedd49eb922dae57b7
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-05-21 09:44:03 +0530

    Create Carvans.java

---
+++ Carvans.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    int a[]=new int[n];
+		    for(int i=0;i<n;i++)
+		    {
+		        a[i]=sc.nextInt();
+		    }
+		    int count=1;
+		    for(int i=0;i<n-1;i++)
+		    {
+		        if(a[i+1]>a[i])
+		          a[i+1]=a[i];
+		         else
+		           count++;
+		    }
+		    System.out.println(count);
+		}
+	}
+}
